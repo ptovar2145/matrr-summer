6f1406b395f4b4924443e915b542535f5c032423
Paula Tovar <Paula_Tovar1@baylor.edu>
2025-07-01 12:19:57 -0500

    Update eda.py

--- scripts/eda.py
+++ scripts/eda.py
@@ -3,7 +3,7 @@ Author: Paula Tovar
 Title: Exploratory Data Analysis
 Description: Program that computes summary data.
 Date Created: 06/16/25
-Date Last Modified: 06/16/25
+Date Last Modified: 07/01/25
 '''
 
 import pandas as pd
@@ -12,43 +12,41 @@ import pandas as pd
 data_path = "../dat/connected_spreadsheet_MATRR.xlsx"
 data_df = pd.read_excel(data_path)
 
+# functions 
+def counts(df, column):
+    return df.groupby(column).size()
+
+def missing_proportion(df):
+    for col in df.columns:
+        num_na = df[col].isnull().sum()
+        proportion = (num_na / df.shape[0]) * 100
+        print(f"{col} {proportion:.2f}%")
+    return 
+    
 # summary data
 print(f"Number of rows: {data_df.shape[0]}")
 print(f"Number of columns: {data_df.shape[1]}")
 print()
 
-category_counts = data_df.groupby("drinking_category").size()
-print(category_counts)
+print(counts(data_df, "drinking_category"))
 print()
 
-species_counts = data_df.groupby("Species").size()
-print(species_counts)
+print(counts(data_df, "Species"))
 print()
 
-sex_counts = data_df.groupby("sex").size()
-print(sex_counts)
+print(counts(data_df, "sex"))
 print()
 
 # proportion of NAs per column
 print("Proportion of NAs per column for all monkeys:")
-for col in data_df.columns:
-    num_na = data_df[col].isnull().sum()
-    proportion = (num_na / data_df.shape[0]) * 100
-    print(f"{col} {proportion:.2f}%")
+missing_proportion(data_df)
 print()
 
 print("Proportion of NAs per column for cyno monkeys:")
 cyno_df = data_df[data_df["Species"] == "cyno"]
-for col in cyno_df.columns:
-    num_na = cyno_df[col].isnull().sum()
-    proportion = (num_na / cyno_df.shape[0]) * 100
-    print(f"{col} {proportion:.2f}%")
+missing_proportion(cyno_df)
 print()
 
 print("Proportion of NAs per column for rhesus monkeys:")
 rhesus_df = data_df[data_df["Species"] == "rhesus"]
-for col in rhesus_df.columns:
-    num_na = rhesus_df[col].isnull().sum()
-    proportion = (num_na / rhesus_df.shape[0]) * 100
-    print(f"{col} {proportion:.2f}%")
-
+missing_proportion(rhesus_df)
