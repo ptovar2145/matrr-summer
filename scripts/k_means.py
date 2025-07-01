d8773d51fb3b71c3dbebc5796323a80750f7a372
Paula Tovar <Paula_Tovar1@baylor.edu>
2025-07-01 09:46:27 -0500

    Add files via upload

---
+++ scripts/k_means.py
@@ -0,0 +1,166 @@
+'''
+Author: Paula Tovar
+Title: K-Means Clustering
+Description: Two k-means models, one having 2 clusters and the other with 4. Baseline and Open Access 1 have their
+             own models.
+Date Created: 06/26/25
+Date Last Modified: 06/30/25
+'''
+
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics.cluster import adjusted_rand_score
+import matplotlib.pyplot as plt
+import numpy as np
+
+# read in data
+data_path = "../dat/standardized.xlsx"
+data_df = pd.read_excel(data_path)
+data_df = data_df.set_index('MATRR ID', drop = False)
+
+# dataframes based on timepoint
+columns_to_drop = [0, 1, 2, 3, 4, 5, 6, 7]
+baseline_df = data_df[data_df["Timepoint"] == "baseline"]
+numeric_baseline = baseline_df.drop(baseline_df.columns[columns_to_drop], axis = 1)
+
+open_df = data_df[data_df["Timepoint"] == "open access 1"]
+numeric_open = open_df.drop(open_df.columns[columns_to_drop], axis = 1)
+
+# functions
+def k_means(data, k):
+    k_means = KMeans(n_clusters = k, random_state = 0)
+    labels = k_means.fit_predict(data)
+    return labels, k_means
+
+def rand_index(results_df):
+    labels_true = results_df["truth_labels"]
+    labels_predicted = results_df["cluster"]
+    return adjusted_rand_score(labels_true, labels_predicted)
+
+def inertia(data, start, end):
+    inertia_vals = []
+    for i in range(start, end + 1):
+        label, k_means_obj = k_means(data, i)
+        inertia_k = k_means_obj.inertia_
+        inertia_vals.append(inertia_k)
+    return inertia_vals
+
+def plot(result_df, num_clusters, title):
+    categories = list(result_df["drinking_category"].unique())
+    categories.sort()
+
+    # create counts dict
+    dict = {}
+    for i in categories:
+        cat_df = result_df[result_df["drinking_category"] == i]
+        count0 = (cat_df["cluster"] == 0).sum()
+        count1 = (cat_df["cluster"] == 1).sum()
+        dict[i] = [count0, count1]
+
+        if num_clusters == 4:
+            count2 = (cat_df["cluster"] == 2).sum()
+            count3 = (cat_df["cluster"] == 3).sum()
+            dict[i].append(count2)
+            dict[i].append(count3)
+
+    group_space = 0.35
+    x = np.arange(num_clusters) * (1 + group_space)
+    width = 0.25
+    mult = 0
+
+    fig, ax = plt.subplots()
+
+    for category, count in dict.items():
+        offset = width * mult
+        rects = ax.bar(x + offset, count, width, label = category)
+        ax.bar_label(rects, padding = 3)
+        mult += 1
+
+    ax.set_ylabel("Monkeys")
+    ax.set_title(title)
+    ax.set_xticks(x + 0.37, range(num_clusters))
+    ax.legend(loc = "upper left", ncols = 4)
+    if num_clusters == 2:
+        ax.set_ylim(0, 45)
+    else:
+        ax.set_ylim(0, 25)
+    plt.show()
+
+def truth_labels(result_df, num_clusters):
+    result_df["truth_labels"] = 0
+
+    if num_clusters == 2:
+        result_df.loc[result_df["drinking_category"] == "HD", "truth_label"] = 1
+        result_df.loc[result_df["drinking_category"] == "VHD", "truth_label"] = 1
+
+    else:
+        result_df.loc[result_df["drinking_category"] == "VHD", "truth_label"] = 1
+        result_df.loc[result_df["drinking_category"] == "LD", "truth_label"] = 2
+        result_df.loc[result_df["drinking_category"] == "HD", "truth_label"] = 3
+
+    return result_df
+
+# baseline - K = 2
+baseline_labels_2, baseline_model_2 = k_means(numeric_baseline, 2)
+baseline_centers_2 = baseline_model_2.cluster_centers_
+
+result_data = {"MATRR ID": baseline_df["MATRR ID"],
+               "drinking_category": baseline_df["drinking_category"],
+               "cluster": baseline_labels_2}
+baseline2_results = pd.DataFrame(result_data)
+baseline2_results = truth_labels(baseline2_results, 2)
+plot(baseline2_results, 2, "Baseline Clustering with 2 Clusters")
+print(rand_index(baseline2_results))
+
+# open access 1 - K = 2
+open_labels_2, open_model_2 = k_means(numeric_open, 2)
+open_centers_2 = open_model_2.cluster_centers_
+
+result_data = {"MATRR ID": open_df["MATRR ID"],
+               "drinking_category": open_df["drinking_category"],
+               "cluster": open_labels_2}
+open2_results = pd.DataFrame(result_data)
+open2_results = truth_labels(open2_results, 2)
+plot(open2_results, 2, "Open Access 1 Clustering with 2 Clusters")
+print(rand_index(open2_results))
+
+# baseline - K = 4
+baseline_labels_4, baseline_model_4 = k_means(numeric_baseline, 4)
+baseline_centers_4 = baseline_model_4.cluster_centers_
+
+result_data = {"MATRR ID": baseline_df["MATRR ID"],
+               "drinking_category": baseline_df["drinking_category"],
+               "cluster": baseline_labels_4}
+baseline4_results = pd.DataFrame(result_data)
+baseline4_results = truth_labels(baseline4_results, 4)
+plot(baseline4_results, 4, "Baseline Clustering with 4 Clusters")
+print(rand_index(baseline4_results))
+
+# open access 1 - K = 4
+open_labels_4, open_model_4 = k_means(numeric_open, 4)
+open_centers_4 = open_model_4.cluster_centers_
+
+result_data = {"MATRR ID": open_df["MATRR ID"],
+               "drinking_category": open_df["drinking_category"],
+               "cluster": open_labels_4}
+open4_results = pd.DataFrame(result_data)
+open4_results = truth_labels(open4_results, 4)
+plot(open4_results, 4, "Open Access 1 Clustering with 4 Clusters")
+print(rand_index(open4_results))
+
+# optimal number of clusters
+inertia_baseline = inertia(numeric_baseline, 1, 10)
+inertia_open = inertia(numeric_open, 1, 10)
+
+plt.plot(range(1, 10 + 1), inertia_baseline,
+         marker = "o",
+         linestyle = "--",
+         color = "blue",)
+plt.plot(range(1, 10 + 1), inertia_open,
+         marker = "o",
+         linestyle = "--",
+         color = "red")
+plt.xlabel("K")
+plt.ylabel("Inertia")
+plt.title("Inertia over different number of clusters")
+plt.show()
